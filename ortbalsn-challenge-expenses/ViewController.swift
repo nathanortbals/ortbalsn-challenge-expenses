5cb3e9e5ddf077967c2a7177e19d81a51de5b6db
Nathan Ortbals <nathan.ortbals@gmail.coma>
2019-01-23 12:01:08 -0600

    Initial Commit

---
+++ ortbalsn-challenge-expenses/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ortbalsn-challenge-expenses
+//
+//  Created by Nathan Ortbals on 1/23/19.
+//  Copyright © 2019 Nathan Ortbals. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
